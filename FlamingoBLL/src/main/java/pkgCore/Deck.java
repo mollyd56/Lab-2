49b11a30c5b9c80dc306c045db7be55d351f2b9c
Molly Doyle /UD <36247600+mollyd56@users.noreply.github.com>
2018-03-07 16:44:41 -0500

    Update Deck.java

--- FlamingoBLL/src/main/java/pkgCore/Deck.java
+++ FlamingoBLL/src/main/java/pkgCore/Deck.java
@@ -1,19 +1,47 @@
 package pkgCore;
 
+import java.util.Collections;
+
+import pkgEnum.*;
+
 public class Deck {
 
 	//	TODO: Add 'cards' attribute that is an ArrayList of Card
+	private ArryList<Card> DeckCards = new ArrayList<Card>();
 	
 	
-	//	TODO: Add a contructor that passes in the number of decks, and then populates
-	//			ArrayList<Card> with cards (depending on number of decks).
+	//	TODO: Add a contructor that passes in the number of decks, and then populates ArrayList<Card> with cards (depending on number of decks).
 	
-	//			Example: Deck(1) will build one 52-card deck.  There are 52 different cards
-	//			2 clubs, 3 clubs... Ace clubs, 2 hearts, 3 hearts... Ace hearts, etc
+	public Deck() {
+		
+	for (eSuit eSuit: eSuit.values()) {
+		
+		for (eRank eRank: eRank.values()) {
+			DeckCards.add(new Card(eRank, eSuit))
+		}
+	}
 	
-	//			Deck(2) will create an array of 104 cards.
+	Collections.shuffle(DeckCards);
+		
 	
+	for (Card c: DeckCards) {
+		System.out.print(c.geteSuit););
+		System.out.print(c);
+		}
+	}
 	
-	//	TODO: Add a draw() method that will take a card from the deck and
-	//			return it to the caller
+	//	TODO: Add a draw() method that will take a card from the deck and return it to the caller
+	
+	public Card Draw() throws Exception {
+	
+		if (DeckCards.size()==0) {
+			throw new Exception("Empty Deck");
+		}
+		
+		Card c = DeckCards.get(0);
+		DeckCards.remove(c);
+		return c;
+		
+	}
+	
 }
