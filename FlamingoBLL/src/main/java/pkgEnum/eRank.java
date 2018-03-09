f36faa09d31c1945f2630a5d124565ec8e9d211b
Molly Doyle /UD <36247600+mollyd56@users.noreply.github.com>
2018-03-08 20:04:54 -0500

    Update eRank.java

--- FlamingoBLL/src/main/java/pkgEnum/eRank.java
+++ FlamingoBLL/src/main/java/pkgEnum/eRank.java
@@ -2,13 +2,18 @@ package pkgEnum;
 
 public enum eRank {
 
-	ONE(1), TWO(2), THREE(3), FOUR(4), FIVE(5), SIX(6), SEVEN(7), EIGHT(8), NINE(9), TEN(10), JACK(11), QUEEN(12), KING(
-			13), ACE(14);
+	TWO(2,2,2), THREE(3,3,3), FOUR(4,4,4), FIVE(5,5,5), SIX(6,6,6), SEVEN(7,7,7), EIGHT(8,8,8), NINE(9,9,9), TEN(10,10,10), JACK(11,10,10), QUEEN(12,10,10), KING(
+			13,10,10), ACE(14,1,11);
 
 	private int iRankNbr;
+	private int iCardValueMin;
+	private int iCardValueMax;
+	
 
-	private eRank(int iRankNbr) {
+	private eRank(int iRankNbr, int iCardValueMax, int iCardValueMin) {
 		this.iRankNbr = iRankNbr;
+		this.iCardValueMax = iCardValueMax;
+		this.iCardValueMin = iCardValueMin;
 	}
 
 	public int getiRankNbr() {
